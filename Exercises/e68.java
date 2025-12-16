cc515a293d03295ad399d1fdd619fb96dec96f60
alidz <alidizajian2000@gmail.com>
2025-12-16 10:30:01 +0330

    E_6_8

---
+++ Exercises/e68.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class e68 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("write a word : ");
+        String s = sc.next();
+        for (int i = 0; i < s.length(); i++){
+            System.out.println(s.charAt(i));
+        }
+    }
+}
