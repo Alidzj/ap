0cb0d4344668df54f29273f554a519250f024586
alidz <alidizajian2000@gmail.com>
2025-12-16 10:28:35 +0330

    E_6_1_a

---
+++ Exercises/e61a.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class e61a {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 2 ; i <= 100 ; i++) {
+            if(i%2==0){
+                sum = sum + i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
