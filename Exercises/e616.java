40c280b5bceb502b67c7cde5e9e43c8fdf619914
alidz <alidizajian2000@gmail.com>
2025-12-16 10:30:47 +0330

    E_6_16

---
+++ Exercises/e616.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class e616 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println();
+            for (int j = 1; j <= n; j++) {
+                System.out.print(i*j+"   ");
+            }
+        }
+    }
+}
